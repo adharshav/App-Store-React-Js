5e873c385189bf4e79b9f0e605598be45ceff19e
bolshevic <adarsh2023asus@gmail.com>
2024-01-28 08:58:48 +0000

    first commit

---
+++ src/components/TabItem/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const Tabitem = props => {
+  const {tabDetails, clickTabItem, isActive} = props
+  const {tabId, displayText} = tabDetails
+
+  const onClickTabItem = () => {
+    clickTabItem(tabId)
+  }
+
+  const activeTabClassName = isActive ? 'active-tab-btn' : 'tab-btn'
+
+  return (
+    <li className="tab-list">
+      <button
+        type="button"
+        className={`tab-btn ${activeTabClassName}`}
+        onClick={onClickTabItem}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default Tabitem
